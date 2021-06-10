eb779652dc1b4dc994e53fbd8f4edb029ee57fff
wangweitao <634022773@qq.com>
2021-06-10 15:53:39 +0800

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UsersController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'User';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('昵称'));
+        $grid->column('email', __('邮箱'));
+        $grid->column('email_verified_at', __('邮箱验证时间'));
+        $grid->column('password', __('密码'));
+        $grid->column('remember_token', __('记住我'));
+        $grid->column('created_at', __('创建时间'));
+        $grid->column('updated_at', __('更新时间'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('昵称'));
+        $show->field('email', __('邮箱'));
+        $show->field('email_verified_at', __('邮箱验证时间'));
+        $show->field('password', __('密码'));
+        $show->field('remember_token', __('记住我'));
+        $show->field('created_at', __('创建时间'));
+        $show->field('updated_at', __('更新时间'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->text('name', __('Name'));
+        $form->email('email', __('Email'));
+        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        $form->password('password', __('Password'));
+        $form->text('remember_token', __('Remember token'));
+
+        return $form;
+    }
+}
