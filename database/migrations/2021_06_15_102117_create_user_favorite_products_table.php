e4cadf35c49f86271a6a5393328c04c2c8bbb062
wangweitao <634022773@qq.com>
2021-06-15 10:37:08 +0800

    用户界面 - 商品收藏

---
+++ database/migrations/2021_06_15_102117_create_user_favorite_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFavoriteProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_favorite_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id')->index('user_id')->comment('用户id');
+            $table->unsignedInteger('product_id')->index('product_id')->comment('商品id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_favorite_products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,9 @@ Route::group(['middleware' => 'auth'], function (){
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+
     });
 });
